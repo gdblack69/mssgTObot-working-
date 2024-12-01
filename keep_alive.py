de60085635489953541004ac620026f90d6a62c9
gdblack69 <geetasaaroj@gmail.com>
2024-12-01 16:49:31 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,10 @@
 from flask import Flask
-import os
-import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running..."
+    return "The bot is running!"
 
-def run():
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
-
-def keep_alive():
-    # Start Flask app in a separate thread to avoid blocking the main process
-    t = threading.Thread(target=run)
-    t.start()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)  # Running Flask on port 8080
